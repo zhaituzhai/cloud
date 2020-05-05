3fc454171280ac3bb66869873b9455f4dc3d3d33
zhaituzhai <995132549@qq.com>
2020-05-06 00:08:10 +0800

    gateway filter

---
+++ cloud-gateway/src/main/java/com/zhaojm/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,39 @@
+package com.zhaojm.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author zhaojm
+ * @date 2020/5/5 23:17
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("************come in MyLogGateWayFilter : " + new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("uname");
+        if(null != username) {
+            log.info("error");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
